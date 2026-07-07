0bc6d9356fc294387900352651ce0c2926badab2
Logic4Code <atulyadwivedi8@gmail.com>
2026-07-07 09:22:52 +0530

    Time: 487 ms (14.22%), Space: 8.8 MB (38.09%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    bool safe(vector<vector<char>>& board, int row,int col,char val){
+        for(int i=0;i<board.size();i++){
+            if(board[row][i]==val) return false;
+            if(board[i][col]==val) return false;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val) return false;
+        }
+        return true;
+    }
+  bool solve(vector<vector<char>>& board) {
+        int n=board.size();
+        for(int row=0;row<n;row++){
+            for(int col=0;col<n;col++){
+                if(board[row][col]=='.'){
+                    for(int val=1;val<=9;val++){
+                    char ch = val + '0';
+                    if(safe(board,row,col,ch)) {
+                    board[row][col]=ch;
+                    if(solve(board)) return true;
+                    else board[row][col]='.';
+                    }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }  
+    
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+   solve(board);
+    }
+};
