fadd2c68618655d7f26b604e471f8b15fe6df6f2
Logic4Code <atulyadwivedi8@gmail.com>
2026-07-13 09:30:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        int cnt = 0;
+        while (n > 1) {
+            if (n == 3) n--;
+            else if (n % 2 == 0) n /= 2;
+            else if (n % 4 == 1) n--;
+            else n++;
+            cnt++;
+        }
+        return cnt;  
+    }
+};
