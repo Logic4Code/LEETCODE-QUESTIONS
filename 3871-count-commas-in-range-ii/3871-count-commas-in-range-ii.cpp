1b0b4f4445595bb6596ddb42c85c2d64ec3531b9
Logic4Code <atulyadwivedi8@gmail.com>
2026-09-09 17:35:11 +0530

    Time: 0 ms (100%), Space: 9 MB (84.8%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+  if (n < 1000) return 0;
+
+    std::string s = to_string(n);
+    long long totalSum = 0;
+    for (int i = (int)s.length() - 3; i > 0; i -= 3) {
+        long long x = 1;
+        for (int k = 0; k < (int)s.length() - i; ++k) {
+            x *= 10;
+        }
+        totalSum += (n - x + 1);
+    }
+
+    return totalSum;      
+    }
+};
